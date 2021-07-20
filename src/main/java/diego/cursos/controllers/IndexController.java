b40a11e1727e39587ed4964988083ca63831f205
Diego Tapia Guajardo <practicadtapia@transbank.cl>
2021-07-20 13:33:43 -0400

    Base project.

---
+++ src/main/java/diego/cursos/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package diego.cursos.controllers;
+
+public class IndexController {
+
+    public String index(){
+
+        return "index";
+    }
+
+    public String oupsHandler(){
+        return "notimplemented";
+    }
+}
