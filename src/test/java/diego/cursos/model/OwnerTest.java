f66e318b5242b2f89d66e3a162dd282341db0e79
Diego Tapia Guajardo <practicadtapia@transbank.cl>
2021-07-20 21:00:54 -0400

    JUnit Parameterized Tests - CSV Source.

--- src/test/java/diego/cursos/model/OwnerTest.java
+++ src/test/java/diego/cursos/model/OwnerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -39,10 +40,20 @@ class OwnerTest implements ModelTests {
         System.out.println(val);
     }
 
-    @DisplayName("Value Source Test ")
+    @DisplayName("ENUM Source Test ")
     @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
     @EnumSource(OwnerType.class)
     void enumTest(OwnerType ownerType){
         System.out.println(ownerType);
+    }
+    @DisplayName("CSV Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvSource({
+            "FL, 1, 1",
+            "OH, 2, 2",
+            "MI, 3, 1"
+    })
+    void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println(stateName+" = "+val1+":"+val2);
     }
 }
