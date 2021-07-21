2d05c86ef77b4691a6f5b6ca1d0284f09943f850
Diego Tapia Guajardo <practicadtapia@transbank.cl>
2021-07-20 20:52:00 -0400

    JUnit Parameterized Tests - Value Source and Display Name.

--- src/test/java/diego/cursos/model/OwnerTest.java
+++ src/test/java/diego/cursos/model/OwnerTest.java
@@ -1,7 +1,11 @@
 package diego.cursos.model;
 
 import diego.cursos.ModelTests;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -25,5 +29,12 @@ class OwnerTest implements ModelTests {
                         ));
 
         assertThat(owner.getCity(),is("Key West"));
+    }
+
+    @DisplayName("Value Source Test ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @ValueSource(strings = {"Spring", "Framework", "Diego"})
+    void testValueSource(String val) {
+        System.out.println(val);
     }
 }
